07fe2dd2e11b622e0e172e833693d6ded6bbd117
Anderson Foschiera <foschieraanderson@gmail.com>
2026-09-16 15:21:24 -0300

    feat: Update main file

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,307 @@
-mod config;
+mod app;
+mod helpers;
+mod player;
+mod playlist;
+mod track;
+mod ui;
 
-use color_eyre::Result;
-use crossterm::event::{self, KeyCode};
-use ratatui::Frame;
-use ratatui::layout::{Constraint, Flex, Layout, Rect};
-use ratatui::style::Stylize;
-use ratatui::widgets::Paragraph;
+use crate::helpers::{print_help, show_image};
+use crate::player::AudioPlayer;
+use crate::playlist::Playlist;
+use crate::ui::footer::render_footer;
+use crate::ui::player::render_player;
+use app::App;
+use std::{
+    env, io,
+    path::PathBuf,
+    time::{Duration, Instant},
+};
 
-use crate::config::load_config;
+use crossterm::{
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
+use ratatui::{
+    Frame, Terminal,
+    backend::CrosstermBackend,
+    layout::{Constraint, Layout, Rect},
+};
 
-    ratatui::run(|terminal| {
-        loop {
-            terminal.draw(|frame| render(frame))?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    enable_raw_mode()?;
 
-            if let Some(key) = event::read()?.as_key_press_event() {
+    // ==========================
+    let mut args = env::args();
+
+    let program = args.next().unwrap_or_else(|| "audio-player".to_string());
+
+    let Some(music_directory) = args.next() else {
+        eprintln!("Uso:");
+        eprintln!("  {program} <diretorio-de-musicas>");
+        eprintln!();
+        eprintln!("Exemplo:");
+        eprintln!("  {program} /home/user/Music");
+
+        return Ok(());
+    };
+
+    let music_directory = PathBuf::from(music_directory);
+
+    // --------------------------------------------------------
+    // Event loop
+    // --------------------------------------------------------
+
+    // loop {
+    //     println!();
+    //
+    //     if let Some(track) = playlist.current() {
+    //         // COVER
+    //         // if let Some(cover) = &track.cover {
+    //         //     show_image(&cover)?;
+    //         //     println!();
+    //         // }
+    //
+    //         println!("Faixa: {}", track.title);
+    //         // if let Some(year) = track.year {
+    //         //     println!(
+    //         //         "Artista: {} | Álbum: {} ({})",
+    //         //         track.artist, track.album, year
+    //         //     );
+    //         // } else {
+    //         //     println!("Artista: {} | Álbum: {}", track.artist, track.album);
+    //         // }
+    //
+    //         let year = track
+    //             .year
+    //             .map(|year| format!(" ({year})"))
+    //             .unwrap_or_default();
+    //
+    //         println!("Artista: {} | Álbum: {}{}", track.artist, track.album, year);
+    //     }
+
+    // let status = if player.is_paused() {
+    //     "Pausado"
+    // } else if player.is_empty() {
+    //     "Parado"
+    // } else {
+    //     "Reproduzindo"
+    // };
+    //
+    // println!("Estado: {status}");
+    //
+    // println!("Volume: {:.0}%", player.volume() * 100.0);
+    //
+    // draw_progress(&player);
+    //
+    // print!("Comando: ");
+    // io::stdout().flush()?;
+    //
+    // let mut input = String::new();
+    //
+    // io::stdin().read_line(&mut input)?;
+
+    // ----------------------------------------------------
+    // Auto next
+    // ----------------------------------------------------
+
+    // if player.is_empty() && !player.is_paused() {
+    //     if let Some(track) = playlist.next() {
+    //         let path = track.path.clone();
+    //
+    //         println!("Próxima automaticamente: {}", track.title);
+    //
+    //         player.play_file(&path)?;
+    //     }
+    // }
+    // }
+
+    // ===============================================
+    let mut stdout = io::stdout();
+
+    execute!(stdout, EnterAlternateScreen)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let result = run(&mut terminal, music_directory);
+
+    disable_raw_mode()?;
+
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+
+    terminal.show_cursor()?;
+
+    result
+}
+
+fn run(
+    terminal: &mut Terminal<CrosstermBackend<io::Stdout>>,
+    music_directory: PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut app = App::new();
+
+    let tick_rate = Duration::from_millis(50);
+    let mut last_tick = Instant::now();
+
+    let mut playlist = Playlist::from_directory(&music_directory)?;
+
+    if playlist.len() == 0 {
+        println!(
+            "Nenhum arquivo de áudio encontrado em {}",
+            music_directory.display()
+        );
+
+        return Ok(());
+    }
+
+    app.current_track = playlist.current().cloned();
+
+    // println!("{} arquivo(s) encontrado(s).", playlist.len());
+
+    let mut player = AudioPlayer::new()?;
+
+    // --------------------------------------------------------
+    // Primeira música
+    // --------------------------------------------------------
+
+    if let Some(track) = playlist.current() {
+        let path = track.path.clone();
+
+        player.play_file(&path)?;
+        app.playing = true;
+
+        if let Some(duration) = player.current_duration {
+            app.duration = duration;
+        }
+    }
+
+    let mut cover_changed = true;
+    let mut cover_area = Rect::default();
+
+    loop {
+        terminal.draw(|frame| {
+            cover_area = render(frame, &app);
+        })?;
+
+        let timeout = tick_rate.saturating_sub(last_tick.elapsed());
+
+        if event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
                 match key.code {
-                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Char('q') => {
+                        player.stop();
+                        break;
+                    }
+
+                    KeyCode::Char('p') => {
+                        app.playing = !app.playing;
+                        player.toggle_pause();
+                    }
+
+                    // KeyCode::Left => {
+                    //     app.seek(-5);
+                    // }
+                    //
+                    // KeyCode::Right => {
+                    //     app.seek(5);
+                    // }
+                    KeyCode::Char('s') => {
+                        player.stop();
+                    }
+                    KeyCode::Char('n') => {
+                        if let Some(track) = playlist.next() {
+                            app.reset();
+                            last_tick = Instant::now();
+                            let path = track.path.clone();
+
+                            player.play_file(&path)?;
+
+                            if let Some(duration) = player.current_duration {
+                                app.duration = duration;
+                            }
+                            app.playing = true;
+                            app.current_track = Some(track.clone());
+                            cover_changed = true;
+                        }
+                    }
+                    KeyCode::Char('b') => {
+                        if let Some(track) = playlist.previous() {
+                            app.reset();
+                            last_tick = Instant::now();
+                            let path = track.path.clone();
+
+                            player.play_file(&path)?;
+                            if let Some(duration) = player.current_duration {
+                                app.duration = duration;
+                            }
+                            app.playing = true;
+                            app.current_track = Some(track.clone());
+                            cover_changed = true;
+                        }
+                    }
+                    KeyCode::Char('+') => {
+                        player.increase_volume();
+                    }
+                    KeyCode::Char('-') => {
+                        player.decrease_volume();
+                    }
+                    KeyCode::Char('h') => {
+                        print_help();
+                    }
+
                     _ => {}
                 }
             }
         }
-    })
+
+        if last_tick.elapsed() >= tick_rate {
+            let delta = last_tick.elapsed();
+
+            app.tick(delta);
+
+            last_tick = Instant::now();
+        }
+        if app.elapsed >= app.duration {
+            if let Some(track) = playlist.next() {
+                app.reset();
+                last_tick = Instant::now();
+                let path = track.path.clone();
+
+                player.play_file(&path)?;
+
+                if let Some(duration) = player.current_duration {
+                    app.duration = duration;
+                }
+                app.playing = true;
+                app.current_track = Some(track.clone());
+                cover_changed = true;
+            }
+        }
+
+        if cover_changed {
+            if let Some(track) = &app.current_track {
+                if let Some(cover) = &track.cover {
+                    show_image(cover, cover_area)?;
+                }
+            }
+
+            cover_changed = false;
+        }
+    }
+
+    Ok(())
 }
 
-fn render(frame: &mut Frame) {
+fn render(frame: &mut Frame, app: &App) -> Rect {
     let area = frame.area();
 
-    // ┌──────────────────────────────────────┐
-    // │                                      │
-    // │              CONTENT                 │
-    // │                                      │
-    // ├──────────────────────────────────────┤
-    // │  [q] Sair  [↑↓] Navegar  [Enter] OK │
-    // └──────────────────────────────────────┘
-    let [content_area, footer_area] =
+    let [content, footer] =
         Layout::vertical([Constraint::Fill(1), Constraint::Length(1)]).areas(area);
 
-    render_content(frame, content_area);
-    render_footer(frame, footer_area);
-}
+    let cover = render_player(frame, content, app);
+    render_footer(frame, footer);
 
-fn render_content(frame: &mut Frame, area: Rect) {
-    // Define o tamanho do conteúdo.
-    // O espaço restante é distribuído para os lados.
-    let [content] = Layout::horizontal([Constraint::Fill(1)])
-        .flex(Flex::Center)
-        .spacing(2)
-        .margin(1 / 2)
-        .areas(area);
-
-    // Centraliza também verticalmente.
-    let [content] = Layout::vertical([Constraint::Fill(1)])
-        .flex(Flex::Center)
-        .spacing(2)
-        .margin(1)
-        .areas(content);
-
-    if let Ok(config) = load_config() {
-        let widget = Paragraph::new(format!(
-            "Volume: {}\nSpectrum: {}\nColor: {}\n",
-            config.player.volume, config.spectrum.enabled, config.appearance.color
-        ))
-        .on_black();
-        frame.render_widget(widget, content);
-    };
-}
-
-fn render_footer(frame: &mut Frame, area: Rect) {
-    let footer = Paragraph::new("[q] Sair   [↑↓] Navegar   [Enter] Selecionar").centered();
-    frame.render_widget(footer, area);
+    cover
 }
